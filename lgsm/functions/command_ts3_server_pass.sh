d091d6cfc4afdd735ae78c86c4dfb0bba4807ba6
UltimateByte <robin.labadie@gmail.com>
2016-11-07 19:18:22 +0100

    fix the hotfix

--- lgsm/functions/command_ts3_server_pass.sh
+++ lgsm/functions/command_ts3_server_pass.sh
@@ -54,8 +54,6 @@ if [ "${status}" != "0" ]; then
 	exitbypass="1"
 	command_stop.sh
 	fn_serveradmin_password_set
-	exitbypass="1"
-	command_stop.sh
 	ts3serverpass="0"
 	fn_print_info_nl "Restarting server normally"
 	fn_script_log_info "Restarting server normally"
