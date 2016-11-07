fe29f8d83ea2ff13a13023788c88601d880deb5f
UltimateByte <robin.labadie@gmail.com>
2016-11-07 19:14:03 +0100

    improved ts3server pw change output

--- lgsm/functions/command_ts3_server_pass.sh
+++ lgsm/functions/command_ts3_server_pass.sh
@@ -33,13 +33,12 @@ fn_serveradmin_password_prompt(){
 
 
 fn_serveradmin_password_set(){
-	fn_print_info_nl "Applying new password"
-	fn_script_log_info "Applying new password"
+	fn_print_info_nl "Starting server with new password..."
+	fn_script_log_info "Starting server with new password"
 	sleep 1
 	# Start server in "new password mode"
 	ts3serverpass="1"
 	exitbypass="1"
-	fn_print_info_nl "Starting server with new password"
 	command_start.sh
 	fn_print_ok_nl "Password applied"
 	fn_script_log_pass "New ServerAdmin password applied"
@@ -50,14 +49,16 @@ fn_serveradmin_password_set(){
 check.sh
 fn_serveradmin_password_prompt
 check_status.sh
-exitbypass="1"
 if [ "${status}" != "0" ]; then
 	# Stop any running server
+	exitbypass="1"
 	command_stop.sh
 	fn_serveradmin_password_set
 	exitbypass="1"
 	command_start.sh
 	ts3serverpass="0"
+	fn_print_info_nl "Restarting server normally"
+	fn_script_log_info "Restarting server normally"
 	command_restart.sh
 else
 	fn_serveradmin_password_set
