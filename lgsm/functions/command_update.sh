02a0a6de01047f7831ef9383a077791f82bb408a
MrJefff <jefferysash@gmail.com>
2016-12-27 14:33:32 +0000

    Update command_update.sh
    Testing

--- lgsm/functions/command_update.sh
+++ lgsm/functions/command_update.sh
@@ -22,15 +22,7 @@ elif [ "${gamename}" == "Mumble" ]; then
 else
 	update_steamcmd.sh
 fi
-if [ -d "../zip-updaterauto/" ]; then
-exec ../zip-updaterauto/zipupdater
-fi
-if [ -d "../zip-updater/" ]; then
-echo "Do you want to update oxide (yes/no) ?"
-read answer
-if [ $answer = yes ]
-  then exec ../zip-updater/zipupdater
-fi
+	install_oxide.sh
 fi
 
 core_exit.sh
